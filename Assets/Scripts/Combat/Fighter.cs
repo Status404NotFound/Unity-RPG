7bb1d64a8b7a5cb06d8eef083544bedbb24106f0
Yuriy Rybin <jura0633728096@gmail.com>
2020-08-21 23:06:48 +0300

    first commit

---
+++ Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class Fighter : MonoBehaviour
+    {
+        public void Atack()
+        {
+            print("Take that you short, squat peasant!");
+        }
+    }
+}
